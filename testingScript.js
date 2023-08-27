f83bab62baeffd5d098d8c840b0f13fbb233cbaf
crangel93 <95656805+crangel93@users.noreply.github.com>
2023-08-26 17:06:55 -0700

    Add files via upload
    init

---
+++ testingScript.js
@@ -0,0 +1,36 @@
+get length variable
+
+get what is selected
+
+create variable that looks for each of the 4 uppercase,lowercase,
+symbols, and numbers
+
+
+if(upperCase.isClicked()){
+    caseArray[0]===true;
+
+
+}if(lowerCase.isClicked()){
+caseArray[1]===true;
+
+}if(symbol.isClicked()){
+    caseArray[2]===true;
+}if(number.isClicked()){
+    caseArray[3]===true;
+}
+or
+let caseArray=[];
+caseArray.push(upperCase.isClicked)
+caseArray.push(lowerCase.isClicked)
+caseArray.push(symbolCase.isClicked)
+caseArray.push(numberCase.isClicked)
+
+for caseArray.length(){
+    generatePassword();
+}
+
+
+var selectionNumber=
+
+
+for(var i=0;i<)
